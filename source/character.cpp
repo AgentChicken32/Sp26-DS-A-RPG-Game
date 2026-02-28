c9886b51e5715ea77dc5e4a84f041ae5f7f1786c
samleinen <samuel.leinen@gmail.com>
2026-02-28 14:10:47 -0500

    -BIG UPDATE- -added Actions to the game, these are objects that can be accessed allowing all characters to access and use them -fixed some older code in BattleClass.cpp As always shoot me a message if you have any questions or concerns - Sam

--- source/character.cpp
+++ source/character.cpp
@@ -14,10 +14,13 @@ int Character::get_health() const { return m_stats.health; }
 int Character::get_mana() const { return m_stats.mana; }
 int Character::get_stamina() const { return m_stats.stamina; }
 int Character::get_gold() const { return m_stats.gold; }
+int Character::get_attack() const { return m_stats.attack; }
 
 int Character::get_max_health() const { return m_stats.max_health; }
 int Character::get_max_mana() const { return m_stats.max_mana; }
 int Character::get_max_stamina() const { return m_stats.max_stamina; }
+
+std::array<int, 6> Character::get_action_ids() { return m_stats.actions; };
 
 bool Character::is_alive() const { return m_stats.health > 0; }
 
@@ -67,6 +70,163 @@ void Character::add_gold(int amount) {
     m_stats.gold += amount;
 }
 
+void Character::set_attack(int value) {
+    m_stats.attack = std::max(0, value);
+}
+
+void Character::status_handler(StatusCondition status, int chanceOf) {
+    //variables
+    std::random_device rd;
+    std::mt19937 gen(rd());
+
+    //check current status, return if the input is same as current status
+    if (m_stats.statusCondition == status) { return; }
+
+    //normal->inflicted
+    if (m_stats.statusCondition == StatusCondition::None) {
+        std::uniform_int_distribution<int> inflict(1, chanceOf);
+
+        if (inflict(gen) == 1) {
+            m_stats.statusCondition = status;
+            switch (status) {
+            case StatusCondition::Poison:
+                std::cout << get_name() << " was poisoned!" << std::endl;
+                break;
+            case StatusCondition::Burn:
+                std::cout << get_name() << " was set ablaze!" << std::endl;
+                break;
+            }
+        }
+    }//inflicted->normal
+    else if (m_stats.statusCondition != StatusCondition::None) {
+        std::uniform_int_distribution<int> recoverPoison(1, 5);
+        std::uniform_int_distribution<int> recoverBurning(1, 3);
+
+        switch (m_stats.statusCondition) {
+        case StatusCondition::Poison://Poison: 1/5 chance to lose effect each turn Deals 2 damage per turn.
+            if (recoverPoison(gen) == 1) {
+                m_stats.statusCondition = StatusCondition::None;
+                std::cout << "The poison lost it's effect!" << std::endl;
+            }
+            else {
+                m_stats.health -= 2;
+                std::cout << get_name() << " took 2 poison damage!" << std::endl;
+            }
+
+            break;
+        case StatusCondition::Burn://Burning: 1/3 chance to lose effect, 5 damage per tur
+            if (recoverBurning(gen) == 1) {
+                m_stats.statusCondition = StatusCondition::None;
+                std::cout << "The fire went out!" << std::endl;
+            }
+            else {
+                m_stats.health -= 5;
+                std::cout << get_name() << " took 5 burning damage!" << std::endl;
+            }
+
+            break;
+        }
+    }
+};
+
+void Character::execute_attack(ActionData action, Character* target) {
+    std::string dividerFlourish = "*-------------------------------------------------------*";
+    std::cout << dividerFlourish << std::endl;
+
+    ////////////////
+    //MANA SECTION//
+    ////////////////
+    //subtract mana from total
+    if (action.manaCost < get_mana() && action.category == Magic) {
+        spend_mana(action.manaCost);
+        std::cout << get_name() << " cast " << action.name << " using " << action.manaCost << " points of mana." << std::endl;
+    }
+    else if (action.category == Magic) {//fail spell if not enough mana
+        std::cout << "The spell fizzled out in front of you! (Not enough mana)" << std::endl;
+        return;
+    }
+
+    //////////////////////////
+    //ACCURACY CHECK SECTION//
+    //////////////////////////
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<int> accuracyCheck(0, 100);
+
+    //check if action will hit
+    if (accuracyCheck(gen) > action.accuracy) {
+        std::cout << "The attack missed!" << std::endl;
+        return;
+    }
+
+    ///////////////////
+    //EFFECTS SECTION//
+    ///////////////////
+
+    //iterate through vector of effects
+    for (const auto& effect : action.effects) {
+
+        //check type of effect
+        switch (effect.type) {
+        case Damage:
+            //deal damage, display message
+            target->take_damage(effect.power);
+            std::cout << get_name() << " hit " << target->get_name() << " with " << action.name << " dealing " << effect.power << " damage!" << std::endl;
+            break;
+        case Status:
+            std::cout << "Whoops, no status was inflicted!\nI have to impliment this! I need to rewrite the status condition system :,)" << std::endl;
+            break;
+        case Buff:
+            //not implemented yet
+            break;
+        case Debuff:
+            //nor is this
+            break;
+        }
+    }
+}
+
+std::vector<int> Character::display_actions(Category type) {
+    int counter = 1;//for display
+    int posCounter = 0;//save positions in id array
+    std::vector<int> idPositions;
+
+    //take input type and iterate through list of action ids, find data, and display names
+    for (int id : get_action_ids()) {
+
+        if (actionDatabase[id].category == type) {
+
+            std::cout << counter << ". " << actionDatabase[id].name;
+
+            //display mana cost: ugly for the time being
+            if (type == Magic) {
+                std::cout << "[" << actionDatabase[id].manaCost << "]";
+            }
+
+            //spacing
+            std::cout << "  ";
+
+            //make new line every 2 actions
+            if ((counter % 2) == 0) {
+                std::cout << std::endl;
+            }
+
+            counter++;
+
+            //save positions in id array
+            idPositions.push_back(posCounter);
+        }
+
+        posCounter++;
+    }
+
+    //display back
+    std::cout << counter << ". BACK" << std::endl;
+
+    //returning to make player decisions possible
+    return idPositions;
+}
+
 void Character::clamp_all() {
     m_stats.max_health  = std::max(0, m_stats.max_health);
     m_stats.max_mana    = std::max(0, m_stats.max_mana);
@@ -79,6 +239,7 @@ void Character::clamp_all() {
     m_stats.stamina =
         std::max(0, std::min(m_stats.stamina, m_stats.max_stamina));
 
+    m_stats.attack = std::max(0, m_stats.attack);
     m_stats.gold = std::max(0, m_stats.gold);
 }
 
@@ -88,13 +249,14 @@ Player::Player(const std::string& name)
     : Character(
           name,
           Character::Stats{
-              /*max_health*/ 125,
-              /*health*/ 125,
-              /*max_mana*/ 50,
-              /*mana*/ 50,
-              /*max_stamina*/ 80,
-              /*stamina*/ 80,
-              /*gold*/ 25
+              .max_health = 125,
+              .health = 125,
+              .max_mana = 50,
+              .mana = 50,
+              .max_stamina = 80,
+              .stamina = 80,
+              .gold = 25,
+              .actions = {0, 0, 0, 0}
           }) {}
 
 // --------------------- Enemy ---------------------
@@ -103,29 +265,39 @@ Enemy::Enemy(int level, const std::string& name)
     : Character(
           name,
           Character::Stats{
-            /*max_health*/ std::max(1, 50 + (level * 20)),
-            /*health*/ std::max(1, 50 + (level * 20)),
-            /*max_mana*/ 0,
-            /*mana*/ 0,
-            /*max_stamina*/ 30,
-            /*stamina*/ 30,
-            /*gold*/ 0
+              .max_health = std::max(1, 50 + (level * 20)),
+              .health = std::max(1, 50 + (level * 20)),
+              .max_mana = 0,
+              .mana = 0,
+              .max_stamina = 30,
+              .stamina = 30,
+              .gold = 0
           }),
       m_level(std::max(1, level)) {}
 
 int Enemy::get_level() const {return m_level;}
 
+void Enemy::enemy_ai(std::vector<Character*> heroes, std::vector<Character*> enemies) {
+
+    //can I kill anyone?
+
+    // Am I about to die?
+
+    //choose target
+
+}
+
 // --------------------- NPC ---------------------
 
 NPC::NPC(const std::string& name)
     : Character(
-          name,
-          Character::Stats{
-              /*max_health*/ 100,
-              /*health*/ 100,
-              /*max_mana*/ 0,
-              /*mana*/ 0,
-              /*max_stamina*/ 0,
-              /*stamina*/ 0,
-              /*gold*/ 0
+        name,
+        Character::Stats{
+              .max_health = 100,
+              .health = 100,
+              .max_mana = 0,
+              .mana = 0,
+              .max_stamina = 0,
+              .stamina = 0,
+              .gold = 0
           }) {}
--- source/character.h
+++ source/character.h
@@ -1,10 +1,16 @@
 #pragma once
 
+#include <iostream>
 #include <string>
 #include <algorithm>
+#include <random>
+#include <vector>
+#include "ActionObject.h"
 
 class Character {
 public:
+
+    //Members
     struct Stats {
         int max_health   = 0;
         int health       = 0;
@@ -12,8 +18,12 @@ public:
         int mana         = 0;
         int max_stamina  = 0;
         int stamina      = 0;
+        int attack       = 0;
         int gold         = 0;
+        std::array<int, 6> actions;
+        StatusCondition statusCondition = StatusCondition::None;
     };
+
 
     Character(std::string name, Stats stats);
     virtual ~Character() = default;
@@ -29,10 +39,13 @@ public:
     int  get_mana() const;
     int  get_stamina() const;
     int  get_gold() const;
+    int  get_attack() const;
 
     int  get_max_health() const;
     int  get_max_mana() const;
     int  get_max_stamina() const;
+
+    std::array<int, 6> get_action_ids();
 
     bool is_alive() const;
 
@@ -49,6 +62,18 @@ public:
     bool spend_gold(int amount);
     void add_gold(int amount);
 
+    void set_attack(int value);
+
+    //How to use: status 0 is normal, 1 is poisoned, 2 is burning etc
+    //chanceOf decides what the chance of contracting the status is other than normal, so an input of 1 is 100%, 5 is 20%, 25 is 4%, etc.
+    void status_handler(StatusCondition status, int chanceOf);
+
+    //runs attack action using chosen action against chosen target
+    void execute_attack(ActionData action, Character* target);
+
+    //Displays Actions Character has: Argument is of type Category an enum located in ActionObject.h
+    std::vector<int> display_actions(Category type);
+
 protected:
     void clamp_all();
 
@@ -61,6 +86,9 @@ protected:
 class Player : public Character {
 public:
     explicit Player(const std::string& name);
+
+    
+
 };
 
 // --------------------- Enemy ---------------------
@@ -69,6 +97,9 @@ class Enemy : public Character {
 public:
     explicit Enemy(int level, const std::string& name = "Enemy");
     int get_level() const;
+
+    //input vector of hero party and enemy gang, the function checks game state and makes a decision based on that
+    void enemy_ai(std::vector<Character*> heroes, std::vector<Character*> enemies);
 
 private:
     int m_level = 1;
@@ -79,4 +110,5 @@ private:
 class NPC : public Character {
 public:
     explicit NPC(const std::string& name = "NPC");
+
 };
